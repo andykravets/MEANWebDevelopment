df4f140cd39868db6728e2c60f3ec0f11a4bd90b
Andrii Kravets <andrey.n.kravets@gmail.com>
2015-08-26 22:04:09 +0300

    Chapter06 part1

---
+++ Chapter06/config/env/development.js
@@ -0,0 +1,7 @@
+/**
+ * Created by akravets on 8/23/15.
+ */
+module.exports = {
+    db:'mongodb://localhost/mean-book',
+    sessionSecret: 'developmentSessionSecret'
+};
---
+++ Chapter06/config/strategies/local.js
@@ -0,0 +1,28 @@
+/**
+ * Created by akravets on 8/26/15.
+ */
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    User = require('mongoose').model('User');
+
+module.exports = function() {
+  passport.use(new LocalStrategy(function(username, password, done){
+      User.findOne({
+          username: username
+      }, function(err, user) {
+          if(err) return done(err);
+          if(!user) {
+              return done(null, false, {
+                 message: 'Login or password is incorrect'
+              });
+          }
+          if(!user.authenticate(password)) {
+              return done(null, false, {
+                  message: 'Login or password is incorrect'
+              });
+          }
+
+          return done(null, user);
+      });
+  }));
+};
